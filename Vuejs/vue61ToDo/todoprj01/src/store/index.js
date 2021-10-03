2e07fd11bb052a892abcd219985b18a18496adb4
hannaclairekim <bonjourhanna@naver.com>
2021-10-03 18:03:00 +0900

    20211003 8교시

--- Vuejs/vue61ToDo/todoprj01/src/store/index.js
+++ Vuejs/vue61ToDo/todoprj01/src/store/index.js
@@ -4,8 +4,69 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
+    state: {
+        todoItems: [
+            { id: 1, todo: "영화보기", done: false },
+            { id: 2, todo: "주말 산책", done: true },
+            { id: 3, todo: "ES6 학습", done: false },
+            //{ id: 4, todo: "잠실 야구장", done: false }
+        ]
+    },
+    getters: {
+        todoItems: function(state) {
+            return state.todoItems;
+        }
+    },
+    actions: {
+        addTodo(mutations, newTodoItem) {
+            mutations.commit("addTodo", newTodoItem);
+        },
+        doneToggle(mutations, id) {
+            mutations.commit("doneToggle", id);
+        },
+        removeTodo(mutations, id, idx) {
+            mutations.commit("removeTodo", id, idx);
+        },
+        clearAll(mutations){
+            mutations.commit("clearAll");
+        }
+    },
+    mutations: {
+        addTodo(state, newTodoItem) {
+            // state.인자 = newTodoItem;
+            let maxid = 0;
+            if (state.todoItems.length > 0) {
+                maxid = state.todoItems
+                    .map((item) => item.id)
+                    .reduce((prev, curt) => (prev >= curt ? prev : curt));
+            } else {
+                maxid = 0;
+            }
+            const newid = maxid + 1;
+
+            const todo = {
+                id: newid,
+                todo: newTodoItem,
+                done: false
+            };
+
+            state.todoItems.push( todo );
+        },
+        clearAll: function(state){
+            state.todoItems = [];
+        },
+        doneToggle: function(state, id) {
+            const findIndex = state.todoItems.findIndex(
+                (item) => id === item.id
+            );
+            state.todoItems[findIndex].done = !state.todoItems[findIndex].done;
+        },
+        removeTodo: function(state, id){
+            const findIndex = state.todoItems.findIndex(
+                (item) => id === item.id
+            );
+            state.todoItems.splice(findIndex, 1);
+        }
+    },
     modules: {}
 });
--- Vuejs/vue61ToDo/todoprj01/src/views/Todo.vue
+++ Vuejs/vue61ToDo/todoprj01/src/views/Todo.vue
@@ -40,18 +40,19 @@ import TodoHeader from "../components/todo/TodoHeader.vue";
 import TodoInput from "../components/todo/TodoInput.vue";
 import TodoList from "../components/todo/TodoList.vue";
 import TodoFooter from "../components/todo/TodoFooter.vue";
+import store from "../store/index.js";
 
 export default {
     /* pdtmc^2w */
     props: [],
     data() {
         return {
-            todoItems: [
-                { id: 1, todo: "영화보기", done: false },
-                { id: 2, todo: "주말 산책", done: true },
-                { id: 3, todo: "ES6 학습", done: false },
-                { id: 4, todo: "잠실 야구장", done: false }
-            ]
+            // todoItems: [
+            //     { id: 1, todo: "영화보기", done: false },
+            //     { id: 2, todo: "주말 산책", done: true },
+            //     { id: 3, todo: "ES6 학습", done: false },
+            //     { id: 4, todo: "잠실 야구장", done: false }
+            // ]
         };
     },
     //template: ``,
@@ -59,50 +60,25 @@ export default {
         /* 이벤트 핸들러 등록 + 일반 함수 */
         clearAll: function () {
             // this.$data.todoItems = [];
-            this.$set(this.$data, "todoItems", []);
+            // this.$set(this.$data, "todoItems", []);
+            store.dispatch("clearAll");
         },
         addTodo(newTodoItem /* 자식 컴포넌트에서 보내는 매개변수 */) {
-            // todoItems에서 최대 id 값을 구하는 방법.
-            // 방법1. todoItems.map()과 todoItems.reduce()를 사용하여 max id를 찾는 방법
-            // 방법2. todoItems.reduce() 를 사용하여 newId를 구하는 방법
-            // 방법2. todoItems.reduce() 를 사용하여 최대 id 값을 찾기
-            let maxid = 0;
-            if (this.$data.todoItems.length > 0) {
-                maxid = this.$data.todoItems
-                    .map((item) => item.id)
-                    .reduce((prev, curt) => (prev >= curt ? prev : curt));
-            } else {
-                maxid = 0;
-            }
-            const newid = maxid + 1;
-
-            const todo = {
-                id: newid,
-                todo: newTodoItem,
-                done: false
-            };
-
-            // this.$data.todoItems.push( todo );
-            // this.$data.todoItems[ this.$data.todoItems.length] = todo;
-            this.$set(
-                this.$data.todoItems,
-                this.$data.todoItems.length + "",
-                todo
-            );
+            // store.dispatch("액션명", 넘길값)
+            store.dispatch("addTodo", newTodoItem);
         },
         doneToggle: function (id) {
-            const findIndex = this.$data.todoItems.findIndex(
-                (item) => id === item.id
-            );
-            this.$data.todoItems[findIndex].done =
-                !this.$data.todoItems[findIndex].done;
+            store.dispatch("doneToggle", id);
         },
         removeTodo: function (id) {
             console.log(id);
+            /*
             const findIndex = this.$data.todoItems.findIndex(
                 (item) => id === item.id
             );
             this.$data.todoItems.splice(findIndex, 1);
+            */
+            store.dispatch("removeTodo", id);
 
             event.stopPropagation();
             event.preventDefault();
@@ -116,6 +92,9 @@ export default {
     },
     computed: {
         /* 자동처리 + 동기식. 메서드로 작성. return 필수. */
+        todoItems() {
+            return store.getters.todoItems;
+        }
     },
     watch: {
         /* 자동처리 + 비동기식. data 에 등록된 프로퍼티(변수) 모니터링. 메서드로 작성. 매개변수 필수. 외부 api 호출을 위해서 사용 */
